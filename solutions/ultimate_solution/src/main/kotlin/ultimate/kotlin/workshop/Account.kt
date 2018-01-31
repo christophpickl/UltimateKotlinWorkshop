8e52a96a778b69730531a6a316b9e6ec4dcef4ae
Christoph Pickl <christoph.pickl@erstegroup.com>
2018-01-31 13:27:31 +0100

    fix ultimate solution bug

--- solutions/ultimate_solution/src/main/kotlin/ultimate/kotlin/workshop/Account.kt
+++ solutions/ultimate_solution/src/main/kotlin/ultimate/kotlin/workshop/Account.kt
@@ -2,6 +2,7 @@ package ultimate.kotlin.workshop
 
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,6 +18,9 @@ data class Account(
         private val repo: AccountRepository
 ) {
     fun readAccounts() = repo.findAll().map { it.toAccount() }
+    fun readAccount(id: Long): Account? {
+        return null // TODO implement me
+    }
     fun createAccount(account: Account) = repo.save(account.toAccountJpa().copy(id = 0)).toAccount()
 }
 
@@ -29,9 +33,8 @@ class AccountController(
     @GetMapping(produces = ["application/json"])
     fun getAccounts(user: User) = service.readAccounts()
 
-
-    @GetMapping(produces = ["application/json"])
-    fun getAccount(user: User) = service.readAccounts()
+    @GetMapping(path = ["/{id}"], produces = ["application/json"])
+    fun getAccount(user: User, @PathVariable id: Long) = service.readAccount(id)
 
     @PostMapping(produces = ["application/json"], consumes = ["application/json"])
     fun postAccount(user: User, @RequestBody account: Account) = service.createAccount(account)
